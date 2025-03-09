a68837b7921de33ef4825a727e73f6bebd46cffd
Tanvir <mrtanvir0072@gmail.com>
2025-03-09 14:34:53 +0530

    env

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
 set -o errexit
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Migrate database
 python manage.py migrate
-if[[ $CREATE_SUPERUSER ]];
-then
-    python manage.py createsupperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+
+# Create superuser if the variable is set
+if [[ $CREATE_SUPERUSER ]]; then
+    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
