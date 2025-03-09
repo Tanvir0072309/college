c34390bea0435c5bd7768805937dc789ec577bc9
Tanvir <mrtanvir0072@gmail.com>
2025-03-09 14:48:06 +0530

    neww

--- build.sh
+++ build.sh
@@ -12,5 +12,13 @@ python manage.py migrate
 
 # Create superuser if the variable is set
 if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --no-input --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" --password "$DJANGO_SUPERUSER_PASSWORD"
+    python manage.py createsuperuser --no-input --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL"
+
+    # Set password for superuser
+    python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+user = get_user_model().objects.get(username="$DJANGO_SUPERUSER_USERNAME")
+user.set_password("$DJANGO_SUPERUSER_PASSWORD")
+user.save()
+EOF
 fi
